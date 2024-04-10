e6e866f13135f3de06b008b59d14bf42693e2f6f
Ivan Kraus <ivan.kraus@post.cz>
2024-04-10 11:32:38 +0200

    css

--- ivanssonne/src/components/HomeScreen.tsx
+++ ivanssonne/src/components/HomeScreen.tsx
@@ -1,92 +1,91 @@
-import React, { FormEvent, useContext, useRef, useState } from 'react';
-import { Link } from 'react-router-dom';
-import Dropdown from 'react-dropdown';
+import React, { useContext, useRef, useState } from 'react';
 import { SettingsContext, MeepleColors, SettingsActionTypes, TypeOfGame } from '../providers/SettingsProvider';
 import logo from '../assets/logo.png';
 import styles from './styles/HomeScreen.module.scss';
-import './styles/fonts.css';
 
-const HomeScreen: React.FC = () => {
+const ColorCircle = ({ color, onSelect, selected }) => {
+    const circleStyle = {
+        backgroundColor: color.toLowerCase(),
+        width: '30px',
+        height: '30px',
+        borderRadius: '50%',
+        display: 'inline-block',
+        margin: '5px',
+        border: selected ? '2px solid black !important' : '2px solid white !important',
+        cursor: 'pointer',
+    };
+    
+    return <div style={circleStyle} onClick={() => onSelect(color)} />;
+};
 
+const HomeScreen = () => {
     const settingsContext = useContext(SettingsContext);
 
-    const handleSettingsSubmit = (e: FormEvent) => {
+    const handleSettingsSubmit = (e) => {
         e.preventDefault();
-
-        console.log("first dropdown: " + firstColor)
-        console.log("second dropdown: " + secondColor)
-
-        const fC = firstColor as MeepleColors;
-        const sC = secondColor as MeepleColors;
 
         settingsContext.dispatch({type: SettingsActionTypes.SET_FIRST_NAME, payload: firstName.current?.value ?? "Player1"});
         settingsContext.dispatch({type: SettingsActionTypes.SET_SECOND_NAME, payload: secondName.current?.value ?? "Player2"});
-        settingsContext.dispatch({type: SettingsActionTypes.SET_FIRST_COLOR, payload: fC});
-        settingsContext.dispatch({type: SettingsActionTypes.SET_SECOND_COLOR, payload: sC});
+        settingsContext.dispatch({type: SettingsActionTypes.SET_FIRST_COLOR, payload: firstColor});
+        settingsContext.dispatch({type: SettingsActionTypes.SET_SECOND_COLOR, payload: secondColor});
         settingsContext.dispatch({type: SettingsActionTypes.SET_TYPE_OF_GAME, payload: TypeOfGame.PVP});
 
-        alert("Settings succesfully updated")
+        alert("Nastavení úspěšně aktualizováno");
+    };
 
-    }
+    const firstName = useRef(null);
+    const secondName = useRef(null);
 
-    const firstName = useRef<HTMLInputElement>(null);
-    const secondName = useRef<HTMLInputElement>(null);
+    const [firstColor, setFirstColor] = useState('');
+    const [secondColor, setSecondColor] = useState('');
 
-    const [firstColor, setFirstColor] = useState<string>();
-    const [secondColor, setSecondColor] = useState<string>();
-
-    let firstDropdownOptions = ["BLACK", "BLUE", "GREEN", "RED", "YELLOW"].filter(col => col !== secondColor);
-    let secondDropdownOptions = ["BLACK", "BLUE", "GREEN", "RED", "YELLOW"].filter(col => col !== firstColor);
+    const colorOptions = ["BLACK", "BLUE", "GREEN", "RED", "YELLOW"];
 
     return (
-        /*
-        <div>
-            <h1>Reactssonne</h1>
-            <form onSubmit={(e) => handleSettingsSubmit(e)}>
-                <h2>Settings</h2>
-                <div>
-                    <p>Player 1</p>
-                    <label htmlFor='FirstName'>Name:</label>
-                    <input ref={firstName} id='FirstName' type='text' required />
-                    <Dropdown options={firstDropdownOptions} onChange={(opt) => setFirstColor(opt.value)} />
-                </div>
-                <div>
-                    <p>Player 2</p>
-                    <label htmlFor='SecondName'>Name:</label>
-                    <input ref={secondName} id='SecondName' type='text' required />
-                    <Dropdown options={secondDropdownOptions} onChange={(opt) => setSecondColor(opt.value)} />
-                </div>
-                <button type='submit'>Confirm</button>
-            </form>
-            <Link to="/game">Start Game</Link>
-            <Link to="/about">About</Link>
-        </div>
-        */
-
         <div className={styles["homepage"]}>
             <img src={logo} alt="logo"/>
-            
             <div className={styles["homepage__login"]}>
-                <h2>Settings</h2>
-                <form onSubmit={(e) => handleSettingsSubmit(e)}>
-                    <div>
+                <form onSubmit={handleSettingsSubmit}>
+                    <div className={styles["user"]}>
                         <p>Hráč 1:</p>
                         <div>
-                            <label htmlFor='FirstName'>Jméno:</label>
-                            <input ref={firstName} id='FirstName' type='text' required />
-                            <Dropdown options={firstDropdownOptions} onChange={(opt) => setFirstColor(opt.value)} />
+                            <div className={styles["user__name"]}>
+                                <label htmlFor='FirstName'>Jméno:</label>
+                                <input ref={firstName} id='FirstName' type='text' required />
+                            </div>
+                            <div className={styles["user__color"]}>
+                                <span>Barva:</span>
+                                <div className={styles["bullets"]}>
+                                {colorOptions.map(color => 
+                                    <ColorCircle key={color} color={color} onSelect={setFirstColor} selected={firstColor === color} />
+                                )}
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div>
+                    <div className={styles["user"]}>
                         <p>Hráč 2:</p>
                         <div>
-                            <label htmlFor='SecondName'>Jméno:</label>
-                            <input ref={secondName} id='SecondName' type='text' required />
-                            <Dropdown options={secondDropdownOptions} onChange={(opt) => setSecondColor(opt.value)} />
+                            <div className={styles["user__name"]}>
+                                <label htmlFor='SecondName'>Jméno:</label>
+                                <input ref={secondName} id='SecondName' type='text' required />
+                            </div>
+                            <div className={styles["user__color"]}>
+                                <span>Barva:</span>
+                                    <div className={styles["bullets"]}>
+                                    {colorOptions.map(color => 
+                                        <ColorCircle key={color} color={color} onSelect={setSecondColor} selected={secondColor === color} />
+                                    )}
+                                    </div>
+                            </div>
                         </div>
                     </div>
                     <button type='submit'>Potvrdit</button>
                 </form>
+            </div>
+
+            <div className={styles["homepage__footer"]}>
+                <p>by K&K corporation ©</p>
             </div>
         </div>
     );
