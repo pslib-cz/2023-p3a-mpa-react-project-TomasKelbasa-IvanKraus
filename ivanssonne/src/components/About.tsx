4852008e53add7a4dec3dd2bf392bf8763b8d6ec
Ivan Kraus <ivan.kraus@post.cz>
2024-04-11 09:46:47 +0200

    about page ready

--- ivanssonne/src/components/About.tsx
+++ ivanssonne/src/components/About.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import navod from '../assets/Reactssonne-navod-1.pdf';
+import styles from './styles/About.module.scss';
+import logo from '../assets/logo.png';
 
 const About: React.FC = () => {
     return (
-        <div>
-            <embed src="https://www.mindok.cz/userfiles/files/pravidla/8595558301126_50.pdf" width="900" height="700" type="application/pdf"/>
-            <Link to="/">Back to Home</Link>
+        <div className={styles["about"]}>
+            <img src={logo} className={styles["about__logo"]} alt="logo"/>
+            <embed src={navod} className={styles["about__pdf"]} width="1200" height="1080" type="application/pdf"/>
+            <Link className={styles["button--back"]} to="/">Back to Home</Link>
         </div>
     );
 };
