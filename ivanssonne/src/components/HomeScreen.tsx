80f3949f1a751b10102fa5ca3748df3e436d65a0
Ivan Kraus <ivan.kraus@post.cz>
2024-04-11 09:13:50 +0200

    homepage ready

--- ivanssonne/src/components/HomeScreen.tsx
+++ ivanssonne/src/components/HomeScreen.tsx
@@ -1,9 +1,10 @@
-import React, { useContext, useRef, useState } from 'react';
+import React, { FormEvent, useContext, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Dropdown from 'react-dropdown';
 import { SettingsContext, MeepleColors, SettingsActionTypes, TypeOfGame } from '../providers/SettingsProvider';
 import logo from '../assets/logo.png';
 import styles from './styles/HomeScreen.module.scss';
-
-const ColorCircle = ({ color, onSelect, selected }) => {
+const ColorCircle = ({ color, onSelect, selected, selectable }) => {
     const circleStyle = {
         backgroundColor: color.toLowerCase(),
         width: '30px',
@@ -11,26 +12,40 @@ const ColorCircle = ({ color, onSelect, selected }) => {
         borderRadius: '50%',
         display: 'inline-block',
         margin: '5px',
-        border: selected ? '2px solid black !important' : '2px solid white !important',
-        cursor: 'pointer',
+        border: selected ? '2px solid beige' : '2px solid black',
+        cursor: selectable ? 'pointer' : 'not-allowed',
+        opacity: selectable ? 1 : 0.5,
     };
     
-    return <div style={circleStyle} onClick={() => onSelect(color)} />;
+    const handleClick = () => {
+        if (selectable) {
+            onSelect(color);
+        }
+    };
+
+    return <div style={circleStyle} onClick={handleClick} />;
 };
+
 
 const HomeScreen = () => {
     const settingsContext = useContext(SettingsContext);
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const displaySuccessMessage = () => {
+        setShowSuccessMessage(true);
+        setTimeout(() => {
+            setShowSuccessMessage(false);
+        }, 3000); 
+    };
 
     const handleSettingsSubmit = (e) => {
         e.preventDefault();
-
-        settingsContext.dispatch({type: SettingsActionTypes.SET_FIRST_NAME, payload: firstName.current?.value ?? "Player1"});
-        settingsContext.dispatch({type: SettingsActionTypes.SET_SECOND_NAME, payload: secondName.current?.value ?? "Player2"});
+        settingsContext.dispatch({type: SettingsActionTypes.SET_FIRST_NAME, payload: firstName.current?.value ?? "Hráč1"});
+        settingsContext.dispatch({type: SettingsActionTypes.SET_SECOND_NAME, payload: secondName.current?.value ?? "Hráč2"});
         settingsContext.dispatch({type: SettingsActionTypes.SET_FIRST_COLOR, payload: firstColor});
         settingsContext.dispatch({type: SettingsActionTypes.SET_SECOND_COLOR, payload: secondColor});
         settingsContext.dispatch({type: SettingsActionTypes.SET_TYPE_OF_GAME, payload: TypeOfGame.PVP});
 
-        alert("Nastavení úspěšně aktualizováno");
+        displaySuccessMessage();
     };
 
     const firstName = useRef(null);
@@ -54,11 +69,17 @@ const HomeScreen = () => {
                                 <input ref={firstName} id='FirstName' type='text' required />
                             </div>
                             <div className={styles["user__color"]}>
-                                <span>Barva:</span>
+                            <span>Barva:</span>
                                 <div className={styles["bullets"]}>
-                                {colorOptions.map(color => 
-                                    <ColorCircle key={color} color={color} onSelect={setFirstColor} selected={firstColor === color} />
-                                )}
+                                    {colorOptions.map(color =>
+                                        <ColorCircle
+                                            key={color}
+                                            color={color}
+                                            onSelect={setFirstColor}
+                                            selected={firstColor === color}
+                                            selectable={secondColor !== color}
+                                        />
+                                    )}
                                 </div>
                             </div>
                         </div>
@@ -72,16 +93,30 @@ const HomeScreen = () => {
                             </div>
                             <div className={styles["user__color"]}>
                                 <span>Barva:</span>
-                                    <div className={styles["bullets"]}>
-                                    {colorOptions.map(color => 
-                                        <ColorCircle key={color} color={color} onSelect={setSecondColor} selected={secondColor === color} />
+                                <div className={styles["bullets"]}>
+                                    {colorOptions.map(color =>
+                                        <ColorCircle
+                                            key={color}
+                                            color={color}
+                                            onSelect={setSecondColor}
+                                            selected={secondColor === color}
+                                            selectable={firstColor !== color}
+                                        />
                                     )}
-                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <button type='submit'>Potvrdit</button>
+                    <button className={styles["button--submit"]} type='submit'>Potvrdit</button>
                 </form>
+                {showSuccessMessage && (
+                    <div className={styles["success_message"]}>Aktualizováno</div>
+                )}
+            </div>
+
+            <div className={styles["homepage__menu"]}>
+                <Link to="/game" className={styles["homepage__menu__item"]}>Nová hra</Link>
+                <Link to="/about" className={styles["homepage__menu__item"]}>Návod</Link>
             </div>
 
             <div className={styles["homepage__footer"]}>
